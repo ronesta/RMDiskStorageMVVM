e805a13e4a55cf7421fa3eb8b591a7bc6b5cf396
ronesta <gvozzzd123@gmail.com>
2025-01-30 21:00:32 +0300

    Implementation of architecture MVVM

---
+++ RMDiskStorageMVVM/RMDiskStorageMVVM/Characters/Model/CharacterDataSourceProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterDataSourceProtocol.swift
+//  RMDiskStorageMVVM
+//
+//  Created by Ибрагим Габибли on 30.01.2025.
+//
+
+import Foundation
+import UIKit
+
+protocol CharacterDataSourceProtocol: UITableViewDataSource {
+    var viewModel: CharacterViewModelProtocol? { get set }
+    var networkManager: NetworkManagerProtocol? { get set }
+}
